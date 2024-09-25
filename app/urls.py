6d5c3ba8b2e03840585b9f55166a53c64200ac0b
Volodya <gtapsplus1@yandex.kz>
2024-09-25 10:01:48 +0300

    Очистка ветки main

--- app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import *
-
-urlpatterns = [
-    path('', index),
-    path('roads/<int:road_id>/', road),
-    path('payments/<int:payment_id>/', payment),
-]
